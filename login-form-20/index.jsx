5f3c099ca02fc3b0dc396f9ede70e0d108bba95a
yuvarajeshwar <yuvarajeshwar@gmail.com>
2024-11-05 16:13:20 +0530

    Aesthetic changes

--- login-form-20/index.jsx
+++ login-form-20/index.jsx
@@ -98,7 +98,7 @@ const Login10 = () => {
   return (
     <html lang="en">
       <head>
-        <title>GLR REPORTING</title>
+        <title>GLR TRACKER</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet" />
